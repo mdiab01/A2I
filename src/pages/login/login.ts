15ab2e4219f9cd92c64c80a105350b2e843f0cd0
AlexRingrose <ringrose.alex@gmail.com>
2019-01-13 19:28:14 -0800

    Login inputs bound to service, navigates to home

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,25 +1,29 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { UserServiceProvider } from '../../providers/user-service/user-service';
 
 @IonicPage()
-@Component({
+@Component( {
   selector: 'page-login',
   templateUrl: 'login.html',
-})
+} )
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor ( public _user: UserServiceProvider,
+    public navCtrl: NavController, public navParams: NavParams ) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+  login () {
+    console.log( 'Logging you in ' + this._user.username );
+    this.navCtrl.parent.select( 0 );
+  }
+
+  toReg () {
+    this.navCtrl.parent.select( 2 );
+  }
+
+  ionViewDidLoad () {
+    console.log( 'ionViewDidLoad LoginPage' );
   }
 
 }
